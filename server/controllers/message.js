d05b6bc6ec3ea51855f7ab19387f6c66539190d4
hmunozMac <hmunoz@unrn.edu.ar>
2016-04-03 17:57:40 -0300

    Cambio de Clase

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -1,8 +1,18 @@
 var mongoose = require('mongoose');
 var Message  = mongoose.model('Message');
 
+
+function changeCrossControl(res){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    res.header("Access-Control-Max-Age", "3600");
+    res.header("Access-Control-Allow-Headers", "x-requested-with");
+}
+
 //GET - Return all tvshows in the DB
 exports.findAll = function(req, res) {
+
+    changeCrossControl(res);
     Message.find(function(err, messages) {
         if(err) res.send(500, err.message);
 
@@ -13,6 +23,9 @@ exports.findAll = function(req, res) {
 
 //GET - Return a TVShow with specified ID
 exports.findById = function(req, res) {
+
+    changeCrossControl(res);
+
     Message.findById(req.params.id, function(err, message) {
         if(err) return res.send(500, err.message);
 
@@ -25,6 +38,8 @@ exports.findById = function(req, res) {
 
 //POST - Insert a new TVShow in the DB
 exports.add = function(req, res) {
+    changeCrossControl(res);
+
     console.log('POST');
     console.log(req.body);
 
@@ -42,6 +57,8 @@ exports.add = function(req, res) {
 
 //PUT - Update a register already exists
 exports.update = function(req, res) {
+    changeCrossControl(res);
+
     Message.findById(req.params.id, function(err, message) {
 
         message.autor   = req.body.autor;
@@ -57,6 +74,8 @@ exports.update = function(req, res) {
 
 //DELETE - Delete a TVShow with specified ID
 exports.delete = function(req, res) {
+    changeCrossControl(res);
+
     console.log('DELETE /delete');
     Message.remove({ id: req.params.id}, function(err){
         if(err) return res.send(500, err.message);
